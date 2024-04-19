dc24a84c1e997b62486958910a16dc44f850407b
Dave Njoroge <davenjorge10@gmail.com>
2024-04-19 22:15:02 +0300

    update homepage, architecture, add tw css, add font

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,13 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
-
   <main>
-    <TheWelcome />
+    <div class="container mx-auto my-10">
+      <h1 class="text-4xl font-bold">Hello Dave!</h1>
+      <p class="mt-10 text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem corporis praesentium modi animi dolorem deserunt culpa, perspiciatis at explicabo perferendis aperiam a, suscipit laborum soluta numquam excepturi aliquid pariatur quia.</p>
+    </div>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+  <script setup lang="ts">
+  </script>
